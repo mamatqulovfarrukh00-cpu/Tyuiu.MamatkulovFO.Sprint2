4d13dbbf59b825e630b1c62216862d4f68beb9b9
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-10-09 16:14:36 +0500

    Task4

--- Tyuiu.MamatkulovFO.Sprint2.Task4.V3.Lib/DataService.cs
+++ Tyuiu.MamatkulovFO.Sprint2.Task4.V3.Lib/DataService.cs
@@ -1,23 +1,18 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint2;
 namespace Tyuiu.MamatkulovFO.Sprint2.Task4.V3.Lib
 {
-    public class DataService:ISprint2Task4V3
+    public class DataService : ISprint2Task4V3
     {
         public double Calculate(double x, double y)
         {
             throw new NotImplementedException();
         }
 
-        public double CalculateZ(double x, double y)
+        public double CalculateY(double x, double y)
         {
-            if (x < y - 1)
-            {
-                return y + (y - 1) / (x + 2);
-            }
-            else
-            {
-                return x + 2 * y / (y + 3);
-            }
+            return x < y - 1
+                ? y + (y - 1) / (x + 2)
+                : x + 2 * y * (1.0 / (y + 3));
         }
     }
 }
