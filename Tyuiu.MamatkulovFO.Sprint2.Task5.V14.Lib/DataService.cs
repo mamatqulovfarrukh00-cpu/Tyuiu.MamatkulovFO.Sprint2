f954ef3bf563ec1f23364acc66b284eea041a6ef
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-10-12 15:28:15 +0500

    Task5

--- Tyuiu.MamatkulovFO.Sprint2.Task5.V14.Lib/DataService.cs
+++ Tyuiu.MamatkulovFO.Sprint2.Task5.V14.Lib/DataService.cs
@@ -4,31 +4,23 @@ namespace Tyuiu.MamatkulovFO.Sprint2.Task5.V14.Lib
 { 
     public class DataService:ISprint2Task5V14
     {
-        public object Run()
+        public string Run(int k, int d)
         {
-            int k = 20;
-            int d = 7;
             int dayIndex = (d + k - 1) % 7;
             if (dayIndex == 0) dayIndex = 7;
 
-            string[] days = {
-            "Понедельник",
-            "Вторник",
-            "Среда",
-            "Четверг",
-            "Пятница",
-            "Суббота",
-            "Воскресенье"
-        };
-
-            return "Пятница";
+            switch (dayIndex)
+            {
+                case 1: return "Понедельник";
+                case 2: return "Вторник";
+                case 3: return "Среда";
+                case 4: return "Четверг";
+                case 5: return "Пятница";
+                case 6: return "Суббота";
+                case 7: return "Воскресенье";
+                default: return "";
+            }
         }
-
-        public object? Run(int v1, int v2)
-        {
-            throw new NotImplementedException();
-        }
-
         string ISprint2Task5V14.FindDayName(int k, int d)
         {
             throw new NotImplementedException();
