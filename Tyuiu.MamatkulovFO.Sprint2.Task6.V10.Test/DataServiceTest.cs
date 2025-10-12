bed5ad4a4606188cfa9efced9a5e81dc2a835294
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-10-12 15:47:18 +0500

    Task6

---
+++ Tyuiu.MamatkulovFO.Sprint2.Task6.V10.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MamatkulovFO.Sprint2.Task6.V10.LIb;
+namespace Tyuiu.MamatkulovFO.Sprint2.Task6.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [Fact]
+        public void TestPreviousDay()
+        {
+            var service = new DataService();
+            var result = service.Run(2025, 3, 1);
+            Assert.AreEqual("28.2.2025", result);
+        }
+    }
+}
---
+++ Tyuiu.MamatkulovFO.Sprint2.Task6.V10/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.MamatkulovFO.Sprint2.Task6.V10.LIb;
+namespace Tyuiu.MamatkulovFO.Sprint2.Task6.V10
+{
+    class Program
+    {
+        static void Main()
+        {
+            var service = new DataService();
+            System.Console.WriteLine(service.Run(2025, 3, 1)); // "28.2.2025"
+        }
+    }
+}
