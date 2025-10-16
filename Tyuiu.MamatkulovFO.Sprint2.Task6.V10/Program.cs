26adc8175cd10e370d34c7884aaa0c555aa8e5af
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-10-16 14:47:47 +0500

    Task6task6

--- Tyuiu.MamatkulovFO.Sprint2.Task6.V10/Program.cs
+++ Tyuiu.MamatkulovFO.Sprint2.Task6.V10/Program.cs
@@ -1,25 +1,17 @@
 ﻿using Tyuiu.MamatkulovFO.Sprint2.Task6.V10.Lib;
-namespace Tyuiu.MamatkulovFO.Sprint2.Task6.V10;
-
-    
-
+namespace Tyuiu.MamatkulovFO.Sprint2.Task6.V10
+{
     class Program
     {
         static void Main()
         {
-            Console.Write("g = ");
+            Console.WriteLine("Введите год, месяц и день:");
             int g = int.Parse(Console.ReadLine());
-
-            Console.Write("m = ");
             int m = int.Parse(Console.ReadLine());
-
-            Console.Write("n = ");
             int n = int.Parse(Console.ReadLine());
 
-            var (y, mo, d) = DataService.GetPreviousDay(g, m, n);
-
-            Console.WriteLine($"{y} {mo} {d}");
+            var ds = new DataService();
+            Console.WriteLine("Предыдущий день: " + ds.FindDateOfPreviousDay(g, m, n));
         }
     }
-
-
+}
