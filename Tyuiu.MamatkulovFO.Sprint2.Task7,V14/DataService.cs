bd265c38b7ca8905d699bb1c0b5ee4af33e91f8a
Фаррух Маматкулов <farrukh0308@inbox.ru>
2025-10-30 16:40:59 +0500

    Task7

---
+++ Tyuiu.MamatkulovFO.Sprint2.Task7,V14/DataService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Tyuiu.MamatkulovFO.Sprint2.Task7.V14
+{
+    internal class DataService
+    {
+        public DataService()
+        {
+        }
+
+        internal bool CheckDotInShadedArea(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tyuiu.MamatkulovFO.Sprint2.Task7.V14.Test/DataService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Tyuiu.MamatkulovFO.Sprint2.Task7.V14.Test
+{
+    internal class DataService
+    {
+        public DataService()
+        {
+        }
+
+        internal bool CheckDotInShadedArea(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
